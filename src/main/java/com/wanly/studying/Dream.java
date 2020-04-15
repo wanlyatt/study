f3357ba113ee20cfb9c397a8a6f23b638ceaa1db
wanly <568601925@qq.com>
2020-04-15 22:41:13 +0800

    first

---
+++ src/main/java/com/wanly/studying/Dream.java
@@ -0,0 +1,24 @@
+package com.wanly.studying;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Dream {
+    class t{
+        private int i;
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            ArrayList list = new ArrayList();
+            ArrayList lists = new ArrayList();
+            Date date = new Date();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
